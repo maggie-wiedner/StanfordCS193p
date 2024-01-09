02d33a95797ca16f9c713bb0453ba61662341b1a
Maggie <maggie.wiedner@gmail.com>
2024-01-08 19:30:57 -0800

    Initial Commit

---
+++ StanfordClass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StanfordClass
+//
+//  Created by Maggie Wiedner on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StanfordClass/StanfordClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordClassApp.swift
+//  StanfordClass
+//
+//  Created by Maggie Wiedner on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct StanfordClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
