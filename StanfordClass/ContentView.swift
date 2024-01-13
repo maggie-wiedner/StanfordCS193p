f0091d171ff9cfe331837f609415a780dd6bcf84
maggie-wiedner <107289712+maggie-wiedner@users.noreply.github.com>
2024-01-13 13:00:47 -0800

    github testing

--- StanfordClass/ContentView.swift
+++ StanfordClass/ContentView.swift
@@ -10,14 +10,51 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CardView(Filled: true)
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+        .font(.title)
+        .imageScale(.small)
+        .foregroundColor(.blue)
     }
 }
+    
+
+
+struct CardView: View {
+    var Filled: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if Filled {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:3)
+                VStack {
+                    Text("❄️")
+                    Text("Emoji")
+                        .foregroundColor(Color.yellow)
+                }
+            }
+            else {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.blue)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:3)
+                VStack {
+                    Text("❄️")
+                    Text("Emoji")
+                        .foregroundColor(Color.pink)
+                }
+            }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
