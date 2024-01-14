0eca482b76bb3978d7e34538f57c29ef4de4c950
maggie-wiedner <107289712+maggie-wiedner@users.noreply.github.com>
2024-01-14 12:20:18 -0800

    sandbox view

---
+++ StanfordClass/SandboxView.swift
@@ -0,0 +1,18 @@
+//
+//  SandboxView.swift
+//  StanfordClass
+//
+//  Created by Maggie Wiedner on 1/14/24.
+//
+
+import SwiftUI
+
+struct SandboxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SandboxView()
+}
