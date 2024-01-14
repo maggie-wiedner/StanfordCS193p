9b34bcd14ac14704c70d644d96109e46a3048fc4
maggie-wiedner <107289712+maggie-wiedner@users.noreply.github.com>
2024-01-13 17:28:32 -0800

    grid view

--- StanfordClass/ContentView.swift
+++ StanfordClass/ContentView.swift
@@ -9,71 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     let emojiArray: [String] = ["🎄","🎅","🧤","☃️","🤶","🍪","🍷","🔥","🌟","🦌","🔔"]
-    
     @State var listSize = 4
-    
     
     
     var body: some View {
         VStack {
             cardDetails
+            Spacer()
             cardAdjusters}
-            .padding()}
-    
+        .padding()}
     
     
     var cardDetails: some View {
-        ForEach(0..<listSize, id: \.self)
-        {index in CardView(content: emojiArray[index])}}
-
-
-        
-        
+        LazyVGrid (columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<listSize, id: \.self) {index in
+                CardView(content: emojiArray[index])}}}
+    
+    
     var cardAdjusters: some View {
         HStack{
             Button(
                 action: {listSize -= 1},
                 label: {Image(systemName: "arrowshape.left.circle.fill")})
+            .disabled(listSize <= 1)
             Spacer()
             Button(
                 action: {listSize += 1},
                 label: {Image(systemName: "arrowshape.right.circle.fill")})
-            }
-            .padding()
-            .foregroundColor(.pink)
-            .imageScale(.large)}}
-
-
-
-
-struct CardView: View {
-    @State var Filled: Bool = true
-    let content: String
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            if Filled {
-                base.fill(.blue)
-                base.strokeBorder(lineWidth:3)
-                VStack {
-                    Text(content)
-                        .foregroundColor(Color.yellow)
-                }
-            }
-            else {
-                base.fill(.white)
-                base.strokeBorder(lineWidth:3)
-                VStack {
-                    Text(content)
-                        .foregroundColor(Color.orange)
-                }
-            }
+            .disabled(listSize >= emojiArray.count)
         }
-        .onTapGesture(perform: {
-            Filled = !Filled
-    })
-    }
-}
+        .padding()
+        .foregroundColor(.pink)
+        .imageScale(.large)}
+    
+    
+    
+    struct CardView: View {
+        @State var Filled: Bool = true
+        let content: String
+        var body: some View {
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
+                if Filled {
+                    base.fill(.blue)
+                        .frame(height: 50)
+                    base.strokeBorder(lineWidth:3)
+                    VStack {
+                        Text(content)
+                            .foregroundColor(Color.yellow)
+                    }
+                }
+                else {
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth:3)
+                    VStack {
+                        Text(content)
+                            .foregroundColor(Color.orange)
+                    }
+                }
+            }
+            .onTapGesture(perform: {
+                Filled = !Filled})}}}
 
 
 #Preview {
